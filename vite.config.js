6b209e57949f6af141f426e0df0bfef3e0568ed0
Boris <bobabonanadz@mail.ru>
2021-12-21 15:23:28 +0300

    Add locales

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueI18n from "@intlify/vite-plugin-vue-i18n";
 import path from "path";
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -7,7 +8,12 @@ export default ({ mode }) => {
   // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
   return defineConfig({
     base: process.env.NODE_ENV === "production" ? "/shop-client/" : "/",
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      vueI18n({
+        include: `${path.resolve(__dirname, "src")}/locales/**`,
+      }),
+    ],
     resolve: {
       alias: {
         "@/": `${path.resolve(__dirname, "src")}/`,
